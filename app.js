7c9eb5030ae03a32eac4cfbc7e3f1578e0d7f833
anjalisahu04 <anjali26cs017@satiengg.in>
2024-09-04 22:46:46 +0530

    Today-Complete

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const Key ="d521caa35ba64cb9a06132319240309 "
-const city="Vidisha"
-const url = `https://api.weatherapi.com/v1/current.json?key=${Key}&q=${city}`;
-let img =document.querySelector(".weather-img")
+const Key ="d521caa35ba64cb9a06132319240309 ";
+const search_btn = document.querySelector(".search");
+const city= document.querySelector(".city-name");
 const fetchWeatherData = async()=>{
-    let response =await fetch(url);
-    let data = await response.json()
-    console.log(data)
-    img.src=data.current.condition.icon
+    let city_val = city.value;
+    if(city_val.length == 0){
+        show.innerHTML = `<h3 class="error">Please enter a city name</h3>`;
+    }else{
+        console.log(city.value)
+        const url = `https://api.weatherapi.com/v1/current.json?key=${Key}&q=${city_val}`;
+        let response =await fetch(url);
+        let data = await response.json()
+        console.log(data)
+        img.src=data.current.condition.icon
+    }    
 }
-fetchWeatherData()
+search_btn.addEventListener('click',fetchWeatherData);
